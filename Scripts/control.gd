7d6885425aa2e0c93c453f948f388647d94891b9
Bbulat7804 <muhdhazimnidzam@gmail.com>
2024-12-20 22:27:06 +0800

    New Start Lest Win This Shit

--- Scripts/control.gd
+++ Scripts/control.gd
@@ -1,7 +1,5 @@
 extends Control
 
-var playerScene = preload("res://Scenes/Player.tscn")
-var map1Scene = preload("res://Scenes/Map1.tscn")
 var map1
 var player1
 var player2
@@ -10,7 +8,6 @@ func _ready() -> void:
 	_initializeMap()
 	_initializePlayer()
 	_initalizeKeyBind()
-	_goToMap(map1)
 	pass # Replace with function body.
 
 
@@ -23,30 +20,21 @@ func _initalizeKeyBind() -> void:
 	player1.up = "W"
 	player1.left = "A"
 	player1.right = "D"
-	player1.push = "Q"
 	
 	player2.down = "K"
 	player2.up = "I"
 	player2.left = "J"
 	player2.right = "L"
-	player2.push = "U"
 	pass
 func _initializePlayer() -> void:
 	
-	player1 = playerScene.instantiate()
-	player2 = playerScene.instantiate()
-	map1.position = Vector2(0,0)
-	player1.position = Vector2(500,500)
-	player2.position = Vector2(700,500)
 	
 	pass
 
 func _initializeMap() -> void:
-	map1 = map1Scene.instantiate()
+	
 	pass
 	
 func _goToMap(map) -> void:
-	map.add_child(player1)
-	map.add_child(player2)
-	add_child(map)
+	
 	pass
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,30 +5,12 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
-var pushSpeed = 700
 var down
 var up
 var left
 var right
-var push
-var isPushed = false
-var pushUp : PushArea
-var pushDown : PushArea
-var pushLeft : PushArea
-var pushRight : PushArea
-
-@onready var push_timer: Timer = $PushTimer
-@onready var pushed_timer: Timer = $PushedTimer
-
-func _ready() -> void:
-	_initialize_push_area()
-	pass
 	
 func _physics_process(delta: float) -> void:
-	
-	if isPushed:
-		move_and_slide()
-		pass
 	# Add the gravity.
 	if Input.is_action_pressed(down):
 		velocity.y = SPEED
@@ -46,49 +28,5 @@ func _physics_process(delta: float) -> void:
 		velocity.x = 0
 	if !Input.is_action_pressed(left) and !Input.is_action_pressed(right):
 		velocity.x = 0
-	if Input.is_action_just_pressed(push):
-		_push()
 		
 	move_and_slide()
-
-func _initialize_push_area() -> void:
-	pushUp = get_node("PushUp")
-	pushUp.pushX = 0
-	pushUp.pushY = -pushSpeed
-	pushDown = get_node("PushDown")
-	pushDown.pushX = 0
-	pushDown.pushY = pushSpeed
-	pushLeft = get_node("PushLeft")
-	pushLeft.pushX = -pushSpeed
-	pushLeft.pushY = 0
-	pushRight = get_node("PushRight")
-	pushRight.pushX = pushSpeed
-	pushRight.pushY = 0
-	pass
-
-func _push() -> void:
-	pushUp.pushable = true
-	pushDown.pushable = true
-	pushRight.pushable = true
-	pushLeft.pushable = true
-	push_timer.start()
-
-func _on_push_timer_timeout() -> void:
-	pushUp.pushable = false
-	pushDown.pushable = false
-	pushLeft.pushable = false
-	pushRight.pushable = false
-	pass # Replace with function body.
-
-func _pushed(x:int, y:int):
-	velocity.x = x
-	velocity.y = y
-	isPushed = true
-	pushed_timer.start()
-	pass
-											 
-func _on_pushed_timer_timeout() -> void:
-	velocity.y = 0
-	velocity.x = 0
-	isPushed = false
-	pass # Replace with function body.
