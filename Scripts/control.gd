264693c5f0f283de7f2f81afabf0041d64ae4746
Bbulat7804 <muhdhazimnidzam@gmail.com>
2024-12-21 01:23:35 +0800

    push lagi

--- Scripts/control.gd
+++ Scripts/control.gd
@@ -1,14 +1,21 @@
 extends Control
 
 var playerScene = preload("res://Scenes/Player.tscn")
+var playerCameraScene = preload("res://Scenes/PlayerCamera.tscn")
+var playerCamera
 var map1
 var player1 : Player
 var player2 : Player
-# Called when the node enters the scene tree for the first time.
+# Called when the node enters the scene tree for the first time.L
 func _ready() -> void:
 	_initializeMap()
 	_initializePlayer()
 	_initalizeKeyBind()
+	player1.position = Vector2(200,500)
+	player2.position = Vector2(400,500)
+	add_child(player1)
+	add_child(player2)
+	
 	pass # Replace with function body.
 
 
@@ -20,14 +27,18 @@ func _initalizeKeyBind() -> void:
 	player1.jump = "W"
 	player1.left = "A"
 	player1.right = "D"
+	player1.character = "1"
 	
-	player1.jump = "I"
-	player1.left = "J"
-	player1.right = "L"
+	player2.jump = "I"
+	player2.left = "J"
+	player2.right = "L"
+	player2.character = "1"
 	pass
 func _initializePlayer() -> void:
-	player1 = 
-	
+	player1 = playerScene.instantiate()
+	player2 = playerScene.instantiate()
+	playerCamera = playerCameraScene.instantiate()
+	player1.add_child(playerCamera)
 	pass
 
 func _initializeMap() -> void:
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,12 +2,14 @@ class_name Player
 
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var animation : AnimatedSprite2D = $AnimatedSprite2D
+const SPEED = 200.0
+const JUMP_VELOCITY = -270.0
 var jump
 var right
 var left
+var direction
+var character
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -15,13 +17,35 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed(Jump) and is_on_floor():
+	if Input.is_action_just_pressed(jump) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	if Input.is_action_just_pressed(Left):
-		velocity.x = -SPEED
-	if Input.is_action_just_pressed(Right):
-		velocity.x = SPEED
-	# Get the input direction and handle the movement/deceleration.
+		animation.play("run" + character)
+	# Get the input direction and handle the movement/deceleration
 	# As good practice, you should replace UI actions with custom gameplay actions.
 
-	move_and_slide()
+	direction = Input.get_axis(left, right)
+	if direction>0:
+		velocity.x = SPEED * 1
+	elif direction<0:
+		velocity.x = SPEED * -1
+	else:
+		velocity.x = 0
+	if is_on_floor():
+		if direction>0:
+			animation.play("run" + character)
+			animation.flip_h = false;
+		elif direction<0:
+			animation.play("run" + character)
+			animation.flip_h = true;
+		else:
+			animation.play("idle" + character)
+	else:
+		if velocity.y<0: 
+			animation.play("jump" + character)
+		else:
+			animation.play("jump" + character)
+		
+	move_and_slide()	
+	
+func _die():
+	queue_free()
