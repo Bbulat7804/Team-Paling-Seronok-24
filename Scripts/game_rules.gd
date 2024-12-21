5c34f31f38e82a08e22ae33186acda83942d0621
damjali <adamrazali54@gmail.com>
2024-12-21 15:28:27 +0800

    LATEST ADAM COMPLETE UI

--- Scripts/game_rules.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-@onready var game_rules: Node = %Game_Rules
-@onready var label: Label = %Label
-
-var points = 0
-func add_point():
-	points += 10
-	print(points)
-	label.text = "Score: " + str(points)
--- Scripts/kill_zone.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-
-
-func _on_body_entered(body: Node2D) -> void:
-	if body is Player:
-		body._die()
-	pass # Replace with function body.
