027bb9aa12babf5994e64ba7249bbebb2f3aa003
damjali <adamrazali54@gmail.com>
2024-12-21 15:38:20 +0800

    latest from adam

--- Scripts/MainScreen.gd
+++ Scripts/MainScreen.gd
@@ -1,11 +1,29 @@
-extends AudioStreamPlayer2D
-
+extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(float) -> void:
 	pass
+
+func _on_button_start_game_mouse_entered() -> void:
+	$SoundHover.play()
+
+func _on_button_options_mouse_entered() -> void:
+	$SoundHover.play()
+
+func _on_button_quit_mouse_entered() -> void:
+	$SoundHover.play()
+
+func _on_button_start_game_pressed() -> void:
+	$SoundClick.play()
+	var next_scene_path = "res://Scenes/ModeSelectScreen.tscn"
+	get_tree().change_scene_to_file("res://Scenes/ModeSelectScreen.tscn")
+
+func _on_button_options_pressed() -> void:
+	$SoundClick.play()
+
+func _on_button_quit_pressed() -> void:
+	$SoundClick.play()
