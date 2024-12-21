d8f1ab2aa1e1938e830a6a54dccb78becfa0de9b
c4meerr <ameermhd2005@gmail.com>
2024-12-21 16:49:18 +0800

    repair score

--- Scripts/ingame_stats.gd
+++ Scripts/ingame_stats.gd
@@ -2,6 +2,7 @@ class_name Stats
 
 extends Node2D
 
+
 var p1Score = 0
 var p2Score = 0
 # Called when the node enters the scene tree for the first time.
@@ -11,6 +12,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Player1Score.text = str(p1Score)
-	$Player2Score.text = str(p2Score)
+	$Score1/Panel/Player1Score.text = str(p1Score)
+	$Score2/Panel/Player2Score.text = str(p2Score)
 	pass
